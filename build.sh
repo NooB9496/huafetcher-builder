90f51726a522084a147c5f28bf426122d818ba80
NooB9496 <96725805+NooB9496@users.noreply.github.com>
2025-07-07 23:59:45 +0200

    Update build.sh
    The method of launching commands from PIP Install + Buildozer has been changed. Thanks to this, Buildozer operates within Virtalenv, not from the global system - which solves the problems with PEP 668. There is no more installation of the Buildozer via PIP in the container at the start, because everything happens in Virtalenv during the image build.

--- build.sh
+++ build.sh
@@ -3,30 +3,15 @@
 mkdir -p build
 chmod 777 build
 docker rmi huafetcher-builder || true
-docker build -t huafetcher-builder .
-docker run -it \
-  --platform linux/amd64 \
-  -v huafetcher-builder-buildozer:/home/buildozer \
+docker build --no-cache -t huafetcher-builder .
+docker run -it --platform linux/amd64 \
   -v $PWD/build/:/build \
   huafetcher-builder \
-  bash -c 'set -eux \
-    ; pip install \
-      buildozer \
-      Cython \
-      kivy \
-    ; export PATH=/home/buildozer/.local/bin:$PATH \
-    ; cd /home/buildozer \
-    ; if [ -e huafetcher ] \
-    ; then \
-        cd huafetcher \
-    ;   git checkout master \
-    ;   git reset --hard \
-    ;   git config pull.rebase true \
-    ;   git pull \
-    ; else \
-        git clone https://codeberg.org/rusi/huafetcher.git \
-    ;   cd huafetcher \
-    ; fi \
-    ; buildozer -v android debug \
-    ; cp /home/buildozer/huafetcher/bin/*.apk /build/ \
+  bash -c '
+    source /home/buildozer/venv/bin/activate \
+    && pip install --break-system-packages buildozer Cython kivy \
+    && cd /home/buildozer \
+    && if [ -e huafetcher ]; then cd huafetcher; git checkout master; git reset --hard; git pull; else git clone https://codeberg.org/rusi/huafetcher.git; cd huafetcher; fi \
+    && buildozer -v android debug \
+    && cp /home/buildozer/huafetcher/bin/*.apk /build/
   '

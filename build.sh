49d54a3c5952042eb8a71c27aada49cc782b2d97
NooB9496 <96725805+NooB9496@users.noreply.github.com>
2025-07-08 08:30:49 +0200

    Update build.sh
    Changed to build v0.26

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ docker run -it --platform linux/amd64 \
   bash -c '
     source /home/buildozer/venv/bin/activate \
     && cd /home/buildozer \
-    && if [ -e huafetcher ]; then cd huafetcher; git checkout master; git reset --hard; git pull; else git clone https://codeberg.org/rusi/huafetcher.git; cd huafetcher; fi \
+    && if [ -e huafetcher ]; then cd huafetcher; git checkout v0.26; git reset --hard; git pull; else git clone https://codeberg.org/vanous/huafetcher.git; cd huafetcher; fi \
     && buildozer -v android debug \
     && cp /home/buildozer/huafetcher/bin/*.apk /build/
   '
